5c44717d3892d8216783cae8763374f853a62937
BDCPatrick <90676378+BDCPatrick@users.noreply.github.com>
2025-09-21 12:23:06 +0200

    Update Copryright Comments for Fab

--- Source/BDC_LevelSelector/Private/SLevelSelectorComboBox.cpp
+++ Source/BDC_LevelSelector/Private/SLevelSelectorComboBox.cpp
@@ -1,3 +1,13 @@
+/* Copyright © beginning at 2025 - BlackDevilCreations
+  * Author: Patrick Wenzel
+  * All rights reserved.
+  * 
+  * This file and the corresponding Definition is part of a BlackDevilCreations project and may not be distributed, copied,
+  * or modified without prior written permission from BlackDevilCreations.
+  * 
+  * Unreal Engine and its associated trademarks are property of Epic Games, Inc.
+  * and are used with permission.
+*/
 #include "SLevelSelectorComboBox.h"
 #include "BDC_LevelSelectorSettings.h"
 #include "ContentBrowserModule.h"
