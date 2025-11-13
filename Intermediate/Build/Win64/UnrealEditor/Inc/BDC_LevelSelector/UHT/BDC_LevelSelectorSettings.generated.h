01ea7c63834594c81082bb398fc0aa2c05a9aa74
BDCPatrick <90676378+BDCPatrick@users.noreply.github.com>
2025-11-13 01:29:33 +0100

    5.7 Support

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/BDC_LevelSelector/UHT/BDC_LevelSelectorSettings.generated.h
@@ -0,0 +1,63 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "BDC_LevelSelectorSettings.h"
+
+#ifdef BDC_LEVELSELECTOR_BDC_LevelSelectorSettings_generated_h
+#error "BDC_LevelSelectorSettings.generated.h already included, missing '#pragma once' in BDC_LevelSelectorSettings.h"
+#endif
+#define BDC_LEVELSELECTOR_BDC_LevelSelectorSettings_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class UBDC_LevelSelectorSettings ***********************************************
+struct Z_Construct_UClass_UBDC_LevelSelectorSettings_Statics;
+BDC_LEVELSELECTOR_API UClass* Z_Construct_UClass_UBDC_LevelSelectorSettings_NoRegister();
+
+#define FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUBDC_LevelSelectorSettings(); \
+	friend struct ::Z_Construct_UClass_UBDC_LevelSelectorSettings_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend BDC_LEVELSELECTOR_API UClass* ::Z_Construct_UClass_UBDC_LevelSelectorSettings_NoRegister(); \
+public: \
+	DECLARE_CLASS2(UBDC_LevelSelectorSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/BDC_LevelSelector"), Z_Construct_UClass_UBDC_LevelSelectorSettings_NoRegister) \
+	DECLARE_SERIALIZER(UBDC_LevelSelectorSettings) \
+	static constexpr const TCHAR* StaticConfigName() {return TEXT("Editor");} \
+
+
+
+#define FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h_21_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	UBDC_LevelSelectorSettings(UBDC_LevelSelectorSettings&&) = delete; \
+	UBDC_LevelSelectorSettings(const UBDC_LevelSelectorSettings&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UBDC_LevelSelectorSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UBDC_LevelSelectorSettings); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UBDC_LevelSelectorSettings) \
+	NO_API virtual ~UBDC_LevelSelectorSettings();
+
+
+#define FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h_18_PROLOG
+#define FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h_21_INCLASS_NO_PURE_DECLS \
+	FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class UBDC_LevelSelectorSettings;
+
+// ********** End Class UBDC_LevelSelectorSettings *************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_UE_Projects_Plugin_Builder_5_7_Plugins_BDC_LevelSelector_Source_BDC_LevelSelector_Public_BDC_LevelSelectorSettings_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
