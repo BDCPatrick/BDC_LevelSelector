0ae20e18f6dc470dbfb7f682ce65bb8e7cf469e8
BDC_Patrick <90676378+BDCPatrick@users.noreply.github.com>
2025-11-26 14:29:40 +0100

    Reformat copyright and module structure

--- Source/BDC_LevelSelector/Private/BDC_LevelSelector.cpp
+++ Source/BDC_LevelSelector/Private/BDC_LevelSelector.cpp
@@ -1,10 +1,10 @@
 /* Copyright © beginning at 2025 - BlackDevilCreations
-  * Author: Patrick Wenzel
-  * All rights reserved.
-  * * This file and the corresponding Definition is part of a BlackDevilCreations project and may not be distributed, copied,
-  * or modified without prior written permission from BlackDevilCreations.
-  * * Unreal Engine and its associated trademarks are property of Epic Games, Inc.
-  * and are used with permission.
+* Author: Patrick Wenzel
+* All rights reserved.
+* This file and the corresponding Definition is part of a BlackDevilCreations project and may not be distributed, copied,
+* or modified without prior written permission from BlackDevilCreations.
+* Unreal Engine and its associated trademarks are property of Epic Games, Inc.
+* and are used with permission.
 */
 #include "BDC_LevelSelector.h"
 #include "SLevelSelectorComboBox.h"
@@ -110,4 +110,5 @@ void FBDC_LevelSelectorModule::OnMapOpened(const FString& Filename, bool bAsTemp
 
 #undef LOCTEXT_NAMESPACE
 	
+
 IMPLEMENT_MODULE(FBDC_LevelSelectorModule, BDC_LevelSelector)
