1fc602bc3178c2c2083860405efd54cbbda041f6
BDC_Patrick <90676378+BDCPatrick@users.noreply.github.com>
2025-11-26 14:29:11 +0100

    Restore copyright notice and module interface

--- Source/BDC_LevelSelector/Public/BDC_LevelSelector.h
+++ Source/BDC_LevelSelector/Public/BDC_LevelSelector.h
@@ -1,10 +1,10 @@
 /* Copyright © beginning at 2025 - BlackDevilCreations
-  * Author: Patrick Wenzel
-  * All rights reserved.
-  * * This file and the corresponding Definition is part of a BlackDevilCreations project and may not be distributed, copied,
-  * or modified without prior written permission from BlackDevilCreations.
-  * * Unreal Engine and its associated trademarks are property of Epic Games, Inc.
-  * and are used with permission.
+* Author: Patrick Wenzel
+* All rights reserved.
+* This file and the corresponding Definition is part of a BlackDevilCreations project and may not be distributed, copied,
+* or modified without prior written permission from BlackDevilCreations.
+* Unreal Engine and its associated trademarks are property of Epic Games, Inc.
+* and are used with permission.
 */
 #pragma once
 
@@ -31,4 +31,5 @@ private:
 	void OnPostEngineInit();
 	void RefreshOverlay();
 	TSharedPtr<SLevelSelectorCameraOverlay> OverlayWidget;
+
 };
